4d54367643457cd0c51f2e0b9ba447d173ee24fb
yabr87 <108272773+yabr87@users.noreply.github.com>
2023-02-27 00:15:25 +0200

    fix persist blacklist

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -13,6 +13,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: 'root',
   storage,
+  blacklist: ['filter'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
